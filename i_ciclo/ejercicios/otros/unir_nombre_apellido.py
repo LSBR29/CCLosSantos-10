ec9a6f64e38c452d7eb90618b2844551a0df0155
LSBR29 <santiagobrenesruiz@gmail.com>
2026-02-16 23:18:10 -0600

    Corrección

--- i_ciclo/ejercicios/otros/unir_nombre_apellido.py
+++
@@ -1,9 +0,0 @@
-#Pedimos el nombre y apellido
-nombre = input("Ingrese un nombre: ")
-apellido = input("Ingrese un apellido: ")
-
-#Unimos los datos
-nombre_completo = nombre + " "  + apellido
-
-#Imprimir el nombre completo
-print(f"Nombre completo: {nombre_completo}")
