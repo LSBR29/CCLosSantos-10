3c028bb94e00566d970b88598efd744843a69336
LSBR29 <santiagobrenesruiz@gmail.com>
2026-03-18 14:31:49 -0600

    Mejora código mayor edad

--- i_ciclo/ejercicios/otros/mayor_edad.py
+++ i_ciclo/ejercicios/otros/mayor_edad.py
@@ -4,5 +4,7 @@ edad = int(input("Ingrese una edad: "))
 #Verificamos si es mayor o menor de edad
 if edad >= 18:
     print("Es mayor de edad")
-else:
+elif edad > 0 and edad < 18:
     print("Es menor de edad")
+else: # En caso de que la edad no esté entre 0 y 18 o sea mayor a 18
+    print("Número inválido")
