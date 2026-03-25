7155af399170b2ecfa01a4c604e52eea0cd20e72
LSBR29 <santiagobrenesruiz@gmail.com>
2026-03-25 10:35:04 -0600

    Mejora ejercicio ejemplo OCi

--- i_ciclo/ejercicios/otros/oci_colección_de_sellos.py
+++ i_ciclo/ejercicios/otros/oci_colección_de_sellos.py
@@ -1,12 +1,6 @@
-#Ingresar los números
-numeros = input("Ingrese dos números: ")
-
-#Separar los números
-num1, num2 = numeros.split()
-
-#Convertirlos a enteros
-num1 = int(num1)
-num2 = int(num2)
+#Ingresar los números y convertir a entero
+num1 = int(input())
+num2 = int(input())
 
 #Si la resta es mayor que 0, imprimo la resta
 if num1 - num2 > 0:
@@ -15,3 +9,15 @@ if num1 - num2 > 0:
 #Si la resta es menor que 0, me quedan 0
 elif num1 - num2 < 0:
     print(0)
+
+
+"""Otra manera
+#Si el primero es mayor al segundo se puede hacer la resta
+if num1 > num2:
+    print(num1 - num2)
+
+#Si no el resultado daría negativo y por tanto se entregaron todos
+else:
+    print(0)
+
+"""
